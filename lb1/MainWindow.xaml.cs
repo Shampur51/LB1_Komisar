eb668708071ea4c0e06eaf39c59c89b595613f60
Timur Komusar <komistimur91@gmail.com>
2023-11-09 16:56:17 +0200

    LAB1

---
+++ lb1/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lb1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       // private void Button_Click(object sender, RoutedEventArgs e)
+       // {
+       // }
+
+        private void Butt_Click(object sender, RoutedEventArgs e)
+        {
+
+            quadr q1, q2, q3;
+            int ss1_1, ss1_2, ss1_3, ss1_4, agg1_1, agg1_2, agg1_3, agg1_4, agg2_1, agg2_2, agg2_3, agg2_4;
+
+            try
+            {
+                ss1_1 = Convert.ToInt32(s1_1.Text);
+                ss1_2 = Convert.ToInt32(s1_5.Text);
+                ss1_3 = Convert.ToInt32(s1_3.Text);
+                ss1_4 = Convert.ToInt32(s1_4.Text);
+                ss1_1 = Convert.ToInt32(s1_1.Text);
+
+                if (ss1_1 <= 0 || ss1_2 <= 0 || ss1_3 <= 0 || ss1_4 <= 0)
+                    throw new Exception("Negativ value of segment");
+
+                agg1_1 = Convert.ToInt32(ag1_1.Text);
+                agg1_2 = Convert.ToInt32(ag1_2.Text);
+                agg1_3 = Convert.ToInt32(ag1_3.Text);
+                agg1_4 = Convert.ToInt32(ag1_4.Text);
+
+                if (agg1_1 + agg1_2 + agg1_3 + agg1_4 != 360)
+                    throw new Exception("The values ​​of the angles are not correct");
+                else if ((agg1_1 == agg1_3 && agg1_2 == agg1_4) && (ss1_1 != ss1_3 || ss1_2 != ss1_4))
+                    throw new Exception("Impossible values");
+                else if ((ss1_1 == ss1_3 && ss1_2 == ss1_4) && (agg1_1 != agg1_3 || agg1_2 != agg1_4))
+                    throw new Exception("Impossible values");
+
+                agg2_1 = Convert.ToInt32(ag2_1.Text);
+                agg2_2 = Convert.ToInt32(ag2_2.Text);
+                agg2_3 = Convert.ToInt32(ag2_3.Text);
+                agg2_4 = Convert.ToInt32(ag2_4.Text);
+
+                if (agg2_1 + agg2_2 + agg2_3 + agg2_4 != 360)
+                    throw new Exception("The values ​​of the angles are not correct");
+
+                q1 = new quadr(ss1_1, ss1_2, ss1_3, ss1_4, agg1_1, agg1_2, agg1_3, agg1_4);
+                q2 = new quadr(agg2_1, agg2_2, agg2_3, agg2_4);
+                q3 = new quadr();
+
+                s2_1.Content = q2.S1;
+                s2_2.Content = q2.S2;
+                s2_3.Content = q2.S3;
+                s2_4.Content = q2.S4;
+
+
+                s3_2.Content = q3.S1;
+                s3_3.Content = q3.S2;
+                s3_4.Content = q3.S3;
+                s3_5.Content = q3.S4;
+
+                ag3_1.Content = q3.Ag1;
+                ag3_2.Content = q3.Ag2;
+                ag3_3.Content = q3.Ag3;
+                ag3_4.Content = q3.Ag4;
+
+                if (q1.IsPar())
+                    ch1.Content = "Так";
+                else
+                    ch1.Content = "Ні";
+
+                if (q2.IsPar())
+                    ch2.Content = "Так";
+                else
+                    ch2.Content = "Ні";
+
+                if (q3.IsPar())
+                    ch3.Content = "Так";
+                else
+                    ch3.Content = "Ні";
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            };
+        }
+    }
+}
---
+++ lb1/quadr.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb1
+{
+    internal class quadr
+    {
+        int s1, s2, s3, s4, ag1, ag2, ag3, ag4;
+
+        public quadr(int s1, int s2, int s3, int s4, int ag1, int ag2, int ag3, int ag4)
+        {
+            this.s1 = s1;
+            this.s2 = s2;
+            this.s3 = s3;
+            this.s4 = s4;
+
+            this.ag1 = ag1;  
+            this.ag2 = ag2;
+            this.ag3 = ag3;
+            this.ag4 = ag4;
+        }
+
+        private void RandS()
+        {
+            Random R = new Random();
+
+            this.s1 = R.Next(1, 100);
+            this.s2 = R.Next(1, 100);
+            this.s3 = R.Next(1, 100);
+            this.s4 = R.Next(1, 100);
+        }
+
+        private void RandAg()
+        { 
+            Random R = new Random();
+
+            this.ag1 = R.Next(1, 358);
+            this.ag2 = R.Next(1, 359 - ag1);
+            this.ag3 = R.Next(1, 360 - (ag1 + ag2));
+            this.ag4 = 360 - (ag1 + ag2 + ag3);
+        }
+
+        public quadr( int ag1, int ag2, int ag3, int ag4)
+        {
+
+            if (ag1 == ag3 && ag4 == ag2)
+            {
+                Random R = new Random();
+
+                this.s1 = R.Next(1, 100);
+                this.s3 = this.s1;
+                this.s2 = R.Next(1, 100);
+                this.s4 = this.s2;
+            }
+
+            else
+            {
+                this.ag1 = ag1;
+                this.ag2 = ag2;
+                this.ag3 = ag3;
+                this.ag4 = ag4;
+
+                RandS();
+            }
+            
+        }
+
+        public quadr()
+        {
+            RandS();
+            RandAg();
+        }
+
+        public double S1
+        {
+            get { return this.s1; }
+        }
+
+        public double S2
+        {
+            get { return this.s2; }
+        }
+
+        public double S3
+        {
+            get { return this.s3; }
+        }
+
+        public double S4
+        {
+            get { return this.s4; }
+        }
+
+        public double Ag1
+        {
+            get { return this.ag1; }
+        }
+
+        public double Ag2
+        {
+            get { return this.ag2; }
+        }
+
+        public double Ag3
+        {
+            get { return this.ag3; }
+        }
+
+        public double Ag4
+        {
+            get { return this.ag4; }
+        }
+
+        public bool IsPar()
+        {
+
+            if ((ag1 == ag2 && ag3 == ag4) && (s1 == s3 && s2 == s4))
+                return true;
+
+            else if (ag1 + ag2 == 180 && ag2 + ag3 == 180 && ag3 + ag4 == 180 && ag1 + ag4 == 180)
+                return true;
+
+            double a1 = Math.Sqrt(Math.Pow(s1, 2) + Math.Pow(s2, 2) - (2 * s1 * s2 * Math.Cos(ag2 * Math.PI / 180.0)));
+            double a2 = Math.Sqrt(Math.Pow(s3, 2) + Math.Pow(s4, 2) - (2 * s3 * s4 * Math.Cos(ag3 * Math.PI / 180.0)));
+
+            if(a1 == a2)
+                return true;
+
+            else
+                return false;
+        }
+    }
+}
